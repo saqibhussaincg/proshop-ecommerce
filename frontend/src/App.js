42cec17d62be63b1391c62eb75aa048957aa9bfa
saqibhussaincg <saqibhussaincg@gmail.com>
2022-09-12 23:52:26 +0500

    working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import {Container} from 'react-bootstrap';
 
 
 function App() {
   return (
    <>
+   <Header />
+   <main className="py-3">
+    <Container>
    <h1>Welcome To Pro Shop</h1>
+    </Container>
+   </main>
+   <Footer /> 
    </>
   );
 }
